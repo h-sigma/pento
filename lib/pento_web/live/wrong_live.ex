f547299b6ebfaf312dc3bdc83132f043357eada9
Harshdeep Singh <harsh@aka.al>
2026-03-30 11:28:30 +0000

    introduction finished

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,54 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:", answer: :rand.uniform(10))}
+  end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    IO.inspect(socket.assigns)
+
+    {message, score, answer} =
+      cond do
+        socket.assigns.answer == to_string(guess) |> String.to_integer() ->
+          {
+            "Correct! Try again for more points.",
+            socket.assigns.score + 1,
+            :rand.uniform(10)
+          }
+
+        true ->
+          {
+            "Your guess: #{guess}. Wrong. Guess again.",
+            socket.assigns.score - 1,
+            socket.assigns.answer
+          }
+      end
+
+    IO.inspect({message, score, answer})
+
+    {
+      :noreply,
+      assign(socket, message: message, score: score, answer: answer)
+    }
+  end
+
+  def time(), do: DateTime.utc_now() |> to_string()
+
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: {@score}</h1>
+    <h2>
+      {@message}
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+        <.link href="#" phx-click="guess" phx-value-number={n}>
+          {n}
+        </.link>
+      <% end %>
+    </h2>
+    """
+  end
+end
